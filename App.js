ea06f4761efa66e61879421d80d7c61d228ad1d7
John <limsopheak89@gmail.com>
2023-12-31 11:17:29 +0700

    Fix eslint configuration and update UI components

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 import Screen from './src/screens/index';
 
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-white h-full">
+      <StatusBar />
       <Screen />
     </SafeAreaView>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import {Image, Text, View} from 'react-native';
 
 export default function Navbar() {
   return (
-    <View className="h-12 mt-[25px] w-full flex flex-row justify-between items-center p-2 bg-blue-300">
+    <View className="h-12 w-full flex flex-row justify-between items-center p-2 bg-blue-300">
       <Text className="font-bold text-sm">My App</Text>
       <View className="flex flex-row items-center">
         <Image
