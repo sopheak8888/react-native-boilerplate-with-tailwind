6723fe7ffdf381b5526022bcef86a4dfffe93002
John <limsopheak89@gmail.com>
2023-12-31 13:47:01 +0700

    Update dependencies and UI components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import {Image, Text, View} from 'react-native';
 export default function Navbar() {
   return (
     <View className="h-12 w-full flex flex-row justify-between items-center p-2 bg-blue-300">
-      <Text className="font-bold text-sm">My App</Text>
-      <View className="flex flex-row items-center">
+      <Text className="text-xl font-extrabold">John App</Text>
+      <View className="flex flex-row items-center bg-slate-600 rounded-full">
         <Image
-          className="w-6 h-6"
+          className="w-8 h-8"
           source={{
             uri: 'https://cdn4.iconfinder.com/data/icons/instagram-ui-twotone/48/Paul-18-512.png',
           }}
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,7 +1,47 @@
-import {Text} from 'react-native';
+import React from 'react';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
+
+const data = [
+  {id: '1', title: 'Item 1'},
+  {id: '2', title: 'Item 2'},
+  {id: '3', title: 'Item 3'},
+];
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: '#f8f8f8',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  listItem: {
+    padding: 10,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  listItemText: {
+    fontSize: 18,
+  },
+});
 
 export default function Home() {
   return (
-    <Text className="text-3xl">Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Home</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({item}) => (
+          <View style={styles.listItem}>
+            <Text style={styles.listItemText}>{item.title}</Text>
+          </View>
+        )}
+      />
+    </View>
   );
 }
